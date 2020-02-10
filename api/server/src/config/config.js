db9b9f2276b91bb75da2ab7aa6504d7183bd1c2f
Ahmad Nurazhim <ahmadfatihnurazhim@gmail.com>
2020-02-10 15:23:57 +0700

    change operatorAliases to operatorsAliases

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     "database": `${process.env.DB_NAME}_development`,
     "host": "127.0.0.1",
     "dialect": "postgres",
-    "operatorAliases": 0
+    "operatorsAliases": 0
   },
   "test": {
     "username": process.env.DB_USER,
@@ -15,7 +15,7 @@ module.exports = {
     "database": `${process.env.DB_NAME}_test`,
     "host": "127.0.0.1",
     "dialect": "postgres",
-    "operatorAliases": 0
+    "operatorsAliases": 0
   },
   "production": {
     "username": process.env.DB_USER,
@@ -23,6 +23,6 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
-    "operatorAliases": 0
+    "operatorsAliases": 0
   }
 }
